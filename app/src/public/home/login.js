95924abe84627f8ff19058f41f628c89a07854bf
k941122 <941122k@naver.com>
2024-01-15 00:36:22 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/home/login.js
+++ app/src/public/home/login.js
@@ -18,5 +18,6 @@ function login () {
             "Content-Type": "application/json"
         },
         body:JSON.stringify(req)
-    })
+    }).then((res) => res.json())
+      .thenconsole.log;
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 const output = {
     home: (req,res) => {
         res.render("home/index");
@@ -9,9 +11,29 @@ const output = {
     },
 };
 
+const users = {
+    id: ["woorinIT","나개발","김팀장"],
+    password: ["1234","1234","123456"],
+}
+
 const process = {
     login : (req,res) => {
-        console.log(req.body);
+       const id = req.body.id,
+        password = req.body.passward;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.password[idx]===password) {
+                return res.json({
+                    sucesss : true,
+                })
+            }
+        }
+
+        return res.json ({
+            sucesss: false,
+            msg: "로그인에 실패하셨습니다.",
+        })
     }
 }
 
