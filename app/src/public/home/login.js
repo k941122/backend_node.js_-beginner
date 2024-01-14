603207bf36e3057766205fbdfe2ac6a502dda284
k941122 <941122k@naver.com>
2024-01-14 17:39:11 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/home/login.js
+++ app/src/public/home/login.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("hello");
+const id  = document.querySelector("#id"),
+    password = documnet.querySelector("#password"),
+    loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click",login);
+
+function login () {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+}
